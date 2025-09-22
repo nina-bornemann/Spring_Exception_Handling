b8c378964c735903382e0b9c403a850213d7cfee
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-22 08:53:21 -0700

    add CarController IntegrationTests

---
+++ src/test/java/de/neuefische/springexceptionhandlingtask/CarControllerTest.java
@@ -0,0 +1,37 @@
+package de.neuefische.springexceptionhandlingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CarControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getCarBrand_shouldReturn_IllegalArgumentException() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/cars/Opel"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.content().string("Only 'porsche' allowed"));
+    }
+
+    @Test
+    void getAllCars_shouldReturn_NoSuchElementException() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/cars"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                                                                           {
+                                                                             "errorMessage": "No cars were found."
+                                                                           }
+                                                                           """));
+    }
+}
