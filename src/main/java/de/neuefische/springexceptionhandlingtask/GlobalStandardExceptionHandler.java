333e78bf9bcd4a8b0583eb10d3ab468957bf9caf
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-22 09:04:44 -0700

    add exceptionhandler for standard exceptions

---
+++ src/main/java/de/neuefische/springexceptionhandlingtask/GlobalStandardExceptionHandler.java
@@ -0,0 +1,29 @@
+package de.neuefische.springexceptionhandlingtask;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalStandardExceptionHandler {
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessage handleNullPointerException(NullPointerException e) {
+        return new ErrorMessage("A null value was encountered: " + e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ErrorMessage(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessage handleGenericException(Exception e) {
+        return new ErrorMessage("Unexpected error: " + e.getClass().getSimpleName());
+    }
+
+}
